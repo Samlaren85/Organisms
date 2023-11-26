820c4dd7b4a4937acd5adcd5b3e6438bccf1b26d
StudentKeranen <s2108237@student.hb.se>
2023-11-26 07:54:35 +0100

    Trying to fix the capture of a camera point on click

---
+++ Assets/Scripts/Controllers/OrganismController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class OrganismController : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+
+    [SerializeField] private TextMeshProUGUI _organismInfo;
+    [SerializeField] private GameObject _selectedGameObject;
+
+    public void Awake()
+    {
+        _camera = Camera.main;
+        _organismInfo = FindObjectsOfType<TextMeshProUGUI>().FirstOrDefault(t => t.name.Equals("OrganismInfo"));
+    }
+
+    public void SelectOrganism(InputAction.CallbackContext context)
+    {
+        if (!context.started) return;
+
+        
+        RaycastHit2D hit = Physics2D.GetRayIntersection(_camera.ScreenPointToRay(Mouse.current.position.ReadValue()));
+        Debug.Log($"LeftClick {_camera.ScreenToWorldPoint(Mouse.current.position.ReadValue())}");
+
+        if (!hit.collider)
+        {
+            if(_selectedGameObject != null)
+            {
+                _selectedGameObject = null;
+                _organismInfo.text = string.Empty;
+            }
+        }
+        else if (hit.collider.gameObject.CompareTag("Organism"))
+        {
+            _selectedGameObject = hit.collider.gameObject;
+            _organismInfo.text = _selectedGameObject.name;
+        }
+    }
+
+    public void MoveOrganism(InputAction.CallbackContext context)
+    {
+        if (!context.started || _selectedGameObject.IsUnityNull()) return;
+
+        RaycastHit2D hit = Physics2D.GetRayIntersection(_camera.ScreenPointToRay(Mouse.current.position.ReadValue()));
+        Debug.Log($"RightClick {_camera.ScreenToViewportPoint(Mouse.current.position.ReadValue())}");
+
+        _selectedGameObject.transform.position = _camera.ScreenToViewportPoint(Mouse.current.position.ReadValue());
+    }
+}
